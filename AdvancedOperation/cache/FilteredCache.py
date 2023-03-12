0f75647a77d3861b4c8f28ba01d5e6895b29e068
julis_99 <the.julis99@gmail.com>
2023-03-12 22:59:23 +0100

    Updated FilteredMatrixCache to be used for Matrix Mult

--- AdvancedOperation/cache/FilteredCache.py
+++ AdvancedOperation/cache/FilteredCache.py
@@ -5,6 +5,8 @@ from Other.input import FLOAT_getRangedInput
 
 
 class FilteredMatrixCache:
+    """:var byRow: Contains ALL Vectors that have #rows == refererenced #cols
+    :var byCol: Contains ALL Vectors that have #cols == refererenced #rows"""
     def __init__(self, unfiltered: Cache, mtxS: MatrixSize):
         self.allowed = mtxS
         self.byRow: list[Matrix] = []
@@ -12,9 +14,9 @@ class FilteredMatrixCache:
         self.byExactMatch: list[Matrix] = []
         self.matchingVectors: list[Vector] = []
         for mtx in unfiltered.getMatrixList():
-            if mtx.getRows() == mtxS.rows:
+            if mtx.getRows() == mtxS.getCols():
                 self.byRow.append(mtx)
-            if mtx.getCols() == mtxS.cols:
+            if mtx.getCols() == mtxS.getRows():
                 self.byCol.append(mtx)
             if MatrixSize(mtx) == mtxS:
                 self.byExactMatch.append(mtx)
@@ -49,6 +51,34 @@ class FilteredMatrixCache:
         rIdx = FLOAT_getRangedInput(0, len(self.byExactMatch) - 1, forceInt=True)
         return self.byExactMatch[rIdx]
 
+    def chooseVector(self) -> Vector:
+        print("Choose one of the following Vectors\n\n")
+        for idx, vct in enumerate(self.matchingVectors):
+            print(f"++Vector {idx}: {vct}")
+            print()
+        rIdx = FLOAT_getRangedInput(0, len(self.matchingVectors) - 1, forceInt=True)
+        return self.matchingVectors[rIdx]
+
+    def containsNoRowMatchMtx(self) -> bool:
+        if not self.byRow:
+            return True
+        return False
+
+    def containsNoColMatchMtx(self) -> bool:
+        if not self.byCol:
+            return True
+        return False
+
+    def containsNoExactMatchMtx(self) -> bool:
+        if not self.byExactMatch:
+            return True
+        return False
+
+    def containsNoVectors(self) -> bool:
+        if not self.matchingVectors:
+            return True
+        return False
+
 class FilteredVectorCache:
     def __init__(self, unfiltered: Cache, lng = int):
         """:var matchingMatrixs The Matrixes from Cache which can be used for Matrix-Vector-Multiplication"""
@@ -79,12 +109,12 @@ class FilteredVectorCache:
         rIdx = FLOAT_getRangedInput(0, len(self.matchingVectors) - 1, forceInt=True)
         return self.matchingVectors[rIdx]
 
-    def containsZeroVectors(self) -> bool:
+    def containsNoVectors(self) -> bool:
         if not self.matchingVectors:
             return True
         return False
 
-    def containsZeroMatrixs(self) -> bool:
+    def containsNoMatrixs(self) -> bool:
         if not self.matchingMatrixs:
             return True
         return False
